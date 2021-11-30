62e128285af6278c07088ddc6b7d07d1d00e30e7
Pat12Gamer <73210414+Pat12Gamer@users.noreply.github.com>
2021-11-30 16:19:26 +0400

    Update lol.py

--- lol.py
+++ lol.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import csv
-START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
 browser = webdriver.Chrome("chromedriver.exe")
 browser.get(START_URL)
 time.sleep(10)
